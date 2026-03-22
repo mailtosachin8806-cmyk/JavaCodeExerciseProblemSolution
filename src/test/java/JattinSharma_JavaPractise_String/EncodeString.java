43603c38a4b8d06d93663cbd47289778a1824f8a
Sachin Gharsele <mailtosachin8806@gmail.com>
2026-03-22 10:44:54 +0530

    Add a Code with Hash Map

--- src/test/java/JattinSharma_JavaPractise_String/EncodeString.java
+++ src/test/java/JattinSharma_JavaPractise_String/EncodeString.java
@@ -1,8 +1,15 @@
 package JattinSharma_JavaPractise_String;
 
+import java.net.MulticastSocket;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
 public class EncodeString {
 
-	public static void main(String[] args) {
+	@Test
+	public void EncodeString() {
 		// TODO Auto-generated method stub
 //Question no. 28
 		
@@ -26,6 +33,26 @@ public class EncodeString {
 		sb.append(input.charAt(input.length()-1));
 		sb.append(count);
 		System.out.println(sb);
+	}
+	
+	@Test
+	public void handleEncodeStringWithHanshMap() {
+		
+		String input = "ddmmmttta";
+		char[] inputArray = input.toCharArray();
+		HashMap<Character, Integer>result = new HashMap<Character, Integer>();
+		
+		for(char data : inputArray)
+		{
+			result.put(data, result.getOrDefault(data, 0)+1);
+		}
+		
+		System.out.println(result);
+		
+		for(Map.Entry<Character, Integer> entry : result.entrySet()) {
+			System.out.println(entry.getKey() + "=" + entry.getValue());
+		}
+		
 	}
 
 }
