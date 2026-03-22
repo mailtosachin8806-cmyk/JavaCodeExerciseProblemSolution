31faad7c0057d5b94eeaa080866dbf75c0d7da4e
Sachin Gharsele <mailtosachin8806@gmail.com>
2026-03-22 18:05:32 +0530

    Remove unwanted variable deceleration

--- src/test/java/JattinSharma_JavaPractise_String/ReverseCharacterOfWordsUsingStringBuilder.java
+++ src/test/java/JattinSharma_JavaPractise_String/ReverseCharacterOfWordsUsingStringBuilder.java
@@ -7,36 +7,28 @@ public class ReverseCharacterOfWordsUsingStringBuilder {
 //Question19
 		
 		String input = "This is fun";
-		String words[]= input.split(" ");
-		String result = " ";
-		
-		StringBuilder sb  = new StringBuilder();
-		for(String word:words)
-		{
-		 char[] Character = word.toCharArray();
-		 int left= 0;
-		 int right = Character.length-1;
-		 char temp;
-		 while(left<right)
-		 {
-			 temp= Character[left];
-			 Character[left] = Character[right];
-			 Character[right] = temp;
-			 left++;
-			 right--;		 
-		 }
-		 
-		 
-		 sb.append(Character).append(" ");	
-		 }
-		
-		System.out.println("Original String : " + input );
-		
-		System.out.println("Reverse String : " + sb);
-		
-		
-		
-		
+		String words[] = input.split(" ");
+
+		StringBuilder sb = new StringBuilder();
+		for (String word : words) {
+			char[] Character = word.toCharArray();
+			int left = 0;
+			int right = Character.length - 1;
+			char temp;
+			while (left < right) {
+				temp = Character[left];
+				Character[left] = Character[right];
+				Character[right] = temp;
+				left++;
+				right--;
+			}
+
+			sb.append(Character).append(" ");
+		}
+
+		System.out.println("Original String : " + input);
+
+		System.out.println("Reverse String : " + sb);		
 	}
 
 }
